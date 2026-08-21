447728e705541ef5bee46539916c2bb637b9b861
Sylvain Benner <sylvain@benner.online>
2026-08-21 11:07:58 -0400

    chore: update to xtask v5 (#38)

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -4,13 +4,13 @@ extern crate log;
 use std::time::Instant;
 use tracel_xtask::prelude::*;
 
-#[macros::base_commands(Build, Bump, Check, Compile, Doc, Fix, Publish, Test, Validate)]
+#[macros::base_commands]
 enum Command {}
 
 fn main() -> anyhow::Result<()> {
     let start = Instant::now();
-    let args = init_xtask::<Command>(parse_args::<Command>()?)?;
-    dispatch_base_commands(args)?;
+    let (args, environment) = init_xtask::<Command>(parse_args::<Command>()?)?;
+    dispatch_base_commands(args, environment)?;
     let duration = start.elapsed();
     info!(
         "\x1B[32;1mTime elapsed for the current execution: {}\x1B[0m",
